ace093bdd27e1d6994398e81e7700d4ceaa0f08a
Nil Silva <nil_silva_12@outlook.com>
2020-05-02 15:05:15 +0100

    Formulario de login e logout criados.

--- header.php
+++ header.php
@@ -16,27 +16,32 @@
         <style>
 
             ul {
-            list-style-type: none;
-            margin: 0;
-            padding: 0;
-            overflow: hidden;
-            background-color: #111;
+                list-style-type: none;
+                margin: 0;
+                padding: 0;
+                overflow: hidden;
+                background-color: #111;
             }
 
             li {
-            float: right;
-            }
-
-            li a {
-            display: block;
-            color: white;
-            text-align: center;
-            padding: 14px 16px;
-            text-decoration: none;
+                float: right;
             }
 
             li a:hover {
-            background-color: #000;
+                padding: 14px 16px;
+                background-color: #000;
+            }
+
+            li a {
+                display: block;
+                color: white;
+                text-align: center;
+                padding: 14px 16px;
+                text-decoration: none;
+            }
+
+            .pad{
+                padding: 14px 16px;
             }
 
         </style>
@@ -49,29 +54,43 @@
 
                 <ul>
 
-                    <li style="float:left;">
+                    <li style="float:left; padding: 2px 4px">
                     
                         <a href="#">
 
-                            <img style="max-width:20px; height:auto; padding: 0px;" src="img/logo.png" alt="Logo" />
+                            <img style="max-width:42px; height:auto; background-color: white;" src="img/logo.png" alt="Logo" />
 
                         </a>
                     
                     </li>
 
-                    <li><a href="index.php">Home</a></>
+                    <li class="pad"><a href="index.php">Home</a></>
 
-                    <li><a href="#">Sobre mim</a></li>
+                    <li class="pad"><a href="#">Sobre mim</a></li>
 
-                    <li><a href="#">Contacto</a></li>
+                    <li class="pad"><a href="#">Contacto</a></li>
 
-                    <li><a href="#">Privacidade</a></li>
+                    <li class="pad"><a href="#">Privacidade</a></li>
 
                 </ul>
 
                 <div>
 
                     <form action="includes/login.inc.php" method="post">
+
+                        <input type="text" name="UserName" placeholder="Introduzir o nome de utilizador"/>
+
+                        <input type="password" name="password" placeholder="Introduzir a password"/>
+
+                        <button type="submit" name="login-submit">Login</button>
+
+                    </form>
+
+                    <a href="signup.php">Signup</a>
+
+                    <form action="includes/logout.inc.php" method="post">
+
+                        <button type="submit" name="logout-submit">Logout</button>
 
                     </form>
 
