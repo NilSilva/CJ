5b1f9c4c7d667d78513fccb966a58ef81dc48780
Nil Silva <nil_silva_12@outlook.com>
2020-05-02 13:55:18 +0100

    Preparações.

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE>
+<html>
+
+    <head>
+
+    <meta charset="utf-8">
+    <meat name="description" content="Exemplo de um site vulnerável a ClickJacking">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>CJ - homepage</title>
+
+    </head>
+
+    <body>
+
+        <header>
+
+        </header>
